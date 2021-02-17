ea0c205d35ee35e0ab3d0d227e7244eee748d897
ANNA MATKOVSKAYA <anuta07.05@gmail.com>
2021-02-17 15:18:56 +0200

    delete

--- Cypress+Udemy/cypress/integration/my_examples/keyboard.spec.js
+++
@@ -1,7 +0,0 @@
-describe('Simulate keyboard press',()=>{
-    it('Should press enter at the keyboard',()=>{
-        cy.visit('http://zero.webappsecurity.com/index.html');
-        cy.get('#searchTerm')
-            .type('some text {enter}');
-    })
-})
